4c299201cf576031324d51af0191e1f31201ce2e
IsraelPeled <ipb14ck0ut@gmail.com>
2025-05-10 23:52:40 +0300

    Adding Authorization

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = function (req, res, next) {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+  if (!token) {
+    console.log("No token");
+    return res.status(401).json({ message: "No token" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("Decoded token:", decoded); // 👈 שים לב לזה
+    req.user = decoded;
+    next();
+  } catch (err) {
+    console.log("Invalid token:", err.message);
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
---
+++ routes/auth.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const User = require("../models/User");
+
+const router = express.Router();
+
+router.post("/signup", async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, email, password: hashedPassword });
+    await user.save();
+    res.status(201).json({ message: "User created seccessfully" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "2h",
+    });
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;

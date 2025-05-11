24ba246528603b80964f29b265cd86315cf6dcc2
IsraelPeled <ipb14ck0ut@gmail.com>
2025-05-11 15:29:54 +0300

    Separate between database connection and app.js

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+
+const connectDB = require("./config/db");
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("✅ Connected to MongoDB"))
-  .catch((err) => console.error("❌ MongoDB connection error:", err));
+connectDB();
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/users", require("./routes/users"));

f222019ddc02a1d2dd57831d5b4560251c433289
IsraelPeled <ipb14ck0ut@gmail.com>
2025-05-11 15:13:03 +0300

    Adding likes

--- models/Post.js
+++ models/Post.js
@@ -6,6 +6,7 @@ const PostSchema = new schema({
   group: { type: schema.Types.ObjectId, ref: "Group" },
   author: { type: schema.Types.ObjectId, ref: "User" },
   content: { type: String, required: true },
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   createAt: { type: Date, default: Date.now },
 });
 

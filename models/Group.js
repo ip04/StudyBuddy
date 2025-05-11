c733cdbce6fb27c40839f0e8686ed7c9a5fc981d
IsraelPeled <ipb14ck0ut@gmail.com>
2025-05-11 17:06:25 +0300

    Adding admins into GroupSchema

--- models/Group.js
+++ models/Group.js
@@ -5,6 +5,7 @@ const schema = mongoose.Schema;
 const GroupSchema = new schema({
   name: { type: String, required: true },
   description: String,
+  admins: [{ type: schema.Types.ObjectId, ref: "User" }],
   members: [{ type: schema.Types.ObjectId, ref: "User" }],
   createdBy: { type: schema.Types.ObjectId, ref: "User" },
   createdAt: { type: Date, default: Date.now },

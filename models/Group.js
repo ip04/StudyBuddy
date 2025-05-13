c528227cbbacc5b6e4f6b3c0b0b7267dd94c91ed
IsraelPeled <ipb14ck0ut@gmail.com>
2025-05-13 22:26:01 +0300

    Adding closed\open group option && Adding modifiers && Adding joinRequests

--- models/Group.js
+++ models/Group.js
@@ -5,8 +5,11 @@ const schema = mongoose.Schema;
 const GroupSchema = new schema({
   name: { type: String, required: true },
   description: String,
+  isClosed: { type: Boolean, default: false },
   admins: [{ type: schema.Types.ObjectId, ref: "User" }],
   members: [{ type: schema.Types.ObjectId, ref: "User" }],
+  modifiers: [{ type: schema.Types.ObjectId, ref: "User" }],
+  joinRequests: [{ type: schema.Types.ObjectId, ref: "User" }],
   createdBy: { type: schema.Types.ObjectId, ref: "User" },
   createdAt: { type: Date, default: Date.now },
 });

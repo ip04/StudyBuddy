a99e3c248e19cd2881bdc8b2a2d3c164e9ca3363
IsraelPeled <ipb14ck0ut@gmail.com>
2025-05-13 22:56:39 +0300

    Adding admins and modifiers group options

---
+++ controllers/adminController.js
@@ -0,0 +1,266 @@
+const Group = require("../models/Group");
+const Post = require("../models/Post");
+
+// approve\decline join requests
+exports.aprroveJoinRequest = async (req, res) => {
+  try {
+    const group = await Group.findById(req.params.groupId);
+    if (!group) return res.status(404).json({ message: "Group not found" });
+
+    if (
+      !group.admins.includes(req.user.id) ||
+      !group.modifiers.includes(req.user.id)
+    ) {
+      return res
+        .status(403)
+        .json({ message: "Only admins or modifiers can perform this action" });
+    }
+
+    if (!group.joinRequests.includes(req.params.userId))
+      return res
+        .status(400)
+        .json({ message: "User is not in the join requests list" });
+
+    group.members.push(req.params.userId);
+
+    group.joinRequests = group.joinRequests.filter(
+      (id) => id.toString() !== req.params.userId
+    );
+
+    await group.save();
+    res.json({ message: "The join request approved" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.declineJoinRequest = async (req, res) => {
+  try {
+    const group = await Group.findById(req.params.groupId);
+    if (!group) return res.status(404).json({ message: "Group not found" });
+
+    if (
+      !group.admins.includes(req.user.id) ||
+      !group.modifiers.includes(req.user.id)
+    ) {
+      return res
+        .status(403)
+        .json({ message: "Only admins or modifiers can perform this action" });
+    }
+
+    if (!group.joinRequests.includes(req.params.userId))
+      return res
+        .status(400)
+        .json({ message: "User is not in the join requests list" });
+
+    group.joinRequests = group.joinRequests.filter(
+      (id) => id.toString() !== req.params.userId
+    );
+
+    await group.save();
+    res.json({ message: "The join request declined" });
+  } catch {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+// remove members or post
+exports.removeMember = async (req, res) => {
+  try {
+    const group = await Group.findById(req.params.groupId);
+    if (!group) return res.status(404).json({ message: "Group not found" });
+
+    const requesterId = req.user.id;
+    const targetId = req.params.userId;
+
+    const isAdmin = group.admins.includes(requesterId);
+    const isModifier = group.modifiers.includes(requesterId);
+
+    const targetIsAdmin = group.admins.includes(targetId);
+    const targetIsModifier = group.modifiers.includes(targetId);
+
+    if (!isAdmin && !isModifier) {
+      return res
+        .status(403)
+        .json({ message: "Only admins or modifiers can perform this action" });
+    }
+
+    if ((targetIsAdmin || targetIsModifier) && !isAdmin) {
+      return res
+        .status(403)
+        .json({ message: "Modifier can't remove admin or other modifier" });
+    }
+
+    if (!group.members.includes(targetId))
+      return res.status(400).json({ message: "The user not in the group" });
+
+    group.members = group.members.filter(
+      (id) => id.toString() !== req.params.userId
+    );
+
+    group.modifiers = group.modifiers.filter(
+      (id) => id.toString() !== req.params.userId
+    );
+
+    await group.save();
+    res.json({ message: "Member was removed successfully" });
+  } catch {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.removePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    if (!post) return res.status(404).json({ message: "Post not found" });
+
+    const group = await Group.findById(post.group);
+    if (!group) return res.status(404).json({ message: "Group not found" });
+
+    const userId = req.user.id;
+    const postAuthorId = post.author.toString();
+
+    const isAdmin = group.admins.includes(userId);
+    const isModifier = group.modifiers && group.modifiers.includes(userId);
+    const isAuthor = postAuthorId === userId;
+
+    const postAuthorIsAdmin = group.admins.includes(postAuthorId);
+    const postAuthorIsModifier =
+      group.modifiers && group.modifiers.includes(postAuthorId);
+
+    if (!isAdmin && !isModifier) {
+      return res
+        .status(403)
+        .json({ message: "Only admins or modifiers can perform this action" });
+    }
+
+    if (
+      isAuthor ||
+      isAdmin ||
+      (isModifier && !postAuthorIsAdmin && !postAuthorIsModifier)
+    ) {
+      await post.deleteOne();
+      return res.json({ message: "Post deleted successfully" });
+    }
+
+    return res
+      .status(403)
+      .json({ message: "Not authorized to delete this post" });
+  } catch {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+// Promotions
+exports.promoteToAdmin = async (req, res) => {
+  try {
+    const group = await Group.findById(req.params.groupId);
+    if (!group) return res.status(404).json({ message: "Group not found" });
+
+    if (!group.admins.includes(req.user.id)) {
+      return res
+        .status(403)
+        .json({ message: "Only admins can perform this action" });
+    }
+
+    if (!group.members.includes(req.params.userId))
+      return res.status(400).json({ message: "The user not in the group" });
+
+    if (group.admins.includes(req.params.userId))
+      return res.status(400).json({ message: "The user already admin" });
+
+    group.admins.push(req.params.userId);
+    await group.save();
+
+    res.json({ message: "The user promoted to admin" });
+  } catch {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.cancelPromoteToAdmin = async (req, res) => {
+  try {
+    const group = await Group.findById(req.params.groupId);
+    if (!group) return res.status(404).json({ message: "Group not found" });
+
+    if (!group.admins.includes(req.user.id)) {
+      return res
+        .status(403)
+        .json({ message: "Only admins can perform this action" });
+    }
+
+    if (!group.members.includes(req.params.userId))
+      return res.status(400).json({ message: "The user not in the group" });
+
+    if (!group.admins.includes(req.params.userId))
+      return res
+        .status(400)
+        .json({ message: "The user not in the admins list" });
+
+    group.admins = group.admins.filter(
+      (id) => id.toString() !== req.params.userId
+    );
+
+    await group.save();
+    res.json({ message: "Member was removed from admins successfully" });
+  } catch {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.promoteToModifier = async (req, res) => {
+  try {
+    const group = await Group.findById(req.params.groupId);
+    if (!group) return res.status(404).json({ message: "Group not found" });
+
+    if (!group.admins.includes(req.user.id)) {
+      return res
+        .status(403)
+        .json({ message: "Only admins can perform this action" });
+    }
+
+    if (!group.members.includes(req.params.userId))
+      return res.status(400).json({ message: "The user not in the group" });
+
+    if (group.modifiers.includes(req.params.userId))
+      return res.status(400).json({ message: "The user already modifier" });
+
+    group.modifiers.push(req.params.userId);
+    await group.save();
+
+    res.json({ message: "The user promoted to modifier" });
+  } catch {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.cancelPromoteToModifier = async (req, res) => {
+  try {
+    const group = await Group.findById(req.params.groupId);
+    if (!group) return res.status(404).json({ message: "Group not found" });
+
+    if (!group.admins.includes(req.user.id)) {
+      return res
+        .status(403)
+        .json({ message: "Only admins can perform this action" });
+    }
+
+    if (!group.members.includes(req.params.userId))
+      return res.status(400).json({ message: "The user not in the group" });
+
+    if (!group.modifiers.includes(req.params.userId))
+      return res
+        .status(400)
+        .json({ message: "The user not in the modifiers list" });
+
+    group.modifiers = group.modifiers.filter(
+      (id) => id.toString() !== req.params.userId
+    );
+
+    await group.save();
+    res.json({ message: "Member was removed from modifiers successfully" });
+  } catch {
+    res.status(500).json({ message: "Server error" });
+  }
+};
---
+++ routes/adminGroup.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const auth = require("../middlewares/authMiddleware");
+const {
+  aprroveJoinRequest,
+  declineJoinRequest,
+  removeMember,
+  promoteToAdmin,
+  cancelPromoteToAdmin,
+  cancelPromoteToModifier,
+  promoteToModifier,
+  removePost,
+} = require("../controllers/adminController");
+
+const router = express.Router();
+
+// Approves a user's join request to the group - accessible by admins and modifiers
+router.post("/:groupId/approve/:userId", auth, aprroveJoinRequest);
+
+// Declines a user's join request to the group – accessible by admins and modifiers
+router.post("/:groupId/decline/:userId", auth, declineJoinRequest);
+
+// Removes a member from the group – accessible by admins and modifiers,
+// but modifier cannot remove admins or other modifiers
+router.delete("/:groupId/remove/:userId", auth, removeMember);
+
+// Deletes a post from the group – accessible by admins and modifiers,
+// but modifiers cannot delete posts created by admins or others modifiers
+router.delete("/:postId/remove", auth, removePost);
+
+// Promotes a group member to admin – accessible by admins only
+router.post("/:groupId/promoteToAdmin/:userId", auth, promoteToAdmin);
+
+// Promotes a group member to modifier – accessible by admins only
+router.post("/:groupId/promoteToModifier/:userId", auth, promoteToModifier);
+
+// Cancels an admin promotion – accessible by admins only
+router.post(
+  "/:groupId/cancelPromoteToAdmin/:userId",
+  auth,
+  cancelPromoteToAdmin
+);
+
+// Cancels a modifier promotion – accessible by admins only
+router.post(
+  "/:groupId/cancelPromoteToModifier/:userId",
+  auth,
+  cancelPromoteToModifier
+);
+
+module.exports = router;
